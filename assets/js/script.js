92e73aa6352df24acdb6a596c00ff1472bcf6fd9
cotebarrientos <cotebarrientos@gmail.com>
2022-07-22 15:13:02 +0100

    Add functionality to the calculator, performing mathematical operations.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,19 @@ const previousValue = document.querySelector('.previous-value');
 const currentValue = document.querySelector('.current-value');
 const operandButtons = document.querySelectorAll('.number');
 const operatorButtons = document.querySelectorAll('.operator');
-const equal = document.querySelector('.equals');
-const allClear = document.querySelector('.all__clear');
-const clear = document.querySelector('.clear');
-const decimal = document.querySelector('.decimal');
+
+const equalBtn = document.querySelector('.equals');
+
+equalBtn.addEventListener('click', () => {
+    // Checking before executing the mathematical operation
+    if(currentNum != "" && previousNum != "") {
+        operate();
+    }
+});
+
+const allClearBtn = document.querySelector('.all__clear');
+const clearBtn = document.querySelector('.clear');
+const decimalBtn = document.querySelector('.decimal');
 
 operandButtons.forEach(btn => {
     btn.addEventListener('click', e => {
@@ -24,4 +33,58 @@ function handleOperand(number) {
         currentValue.textContent = currentNum;
         console.log(number);
     }
+}
+
+operatorButtons.forEach(btn => {
+    btn.addEventListener('click', e => {
+        handleOperator(e.target.textContent);
+    });
+});
+
+function handleOperator(op) {
+    operator = op;
+    previousNum = currentNum;
+    previousValue.textContent = previousNum + " " + operator;
+    currentNum = "";
+    currentValue.textContent = "";
+}
+
+function operate() {
+    previousNum = Number(previousNum);
+    currentNum = Number(currentNum);
+
+    if(operator === "+"){
+        previousNum = previousNum + currentNum;
+    } else if(operator === "-"){
+        previousNum = previousNum - currentNum;
+    } else if(operator === "x"){
+        previousNum = previousNum * currentNum;
+    } else if(operator === "÷"){
+        if(currentNum <= 0) {
+            previousNum = `Error! (ง︡'-'︠)ง`;
+            displayResults();
+            return;
+        }
+        previousNum = previousNum / currentNum;
+    } else if(operator === "%"){
+        previousNum = previousNum /100 * currentNum;
+    }
+
+    previousNum = roundNumber(previousNum);
+    previousNum = previousNum.toString();
+    displayResults();
+}
+
+function displayResults() {
+    previousValue.textContent = "";
+    operator = "";
+    if(previousNum.length <= 11){
+        currentValue.textContent = previousNum;
+    } else {
+        currentValue.textContent = previousNum.slice(0, 16) + "...";
+    }
+}
+
+function roundNumber(num) {
+    return Math.round(num * 100000) / 100000;
 }
